3025843b15d7e6fb4020ecd1c3dd39dbc715402e
rahulyadav26 <36184559+rahulyadav26@users.noreply.github.com>
2021-05-15 20:24:30 +0530

    Update pcc.rb

--- pcc.rb
+++ pcc.rb
@@ -4,7 +4,7 @@
 class Pcc < Formula
   desc ""
   homepage ""
-  url "https://github.com/rahulyadav26/pcc/archive/refs/tags/v0.1.tar.gz"
+  url "https://github.com/rahulyadav26/pcc/archive/refs/tags/v0.2.tar.gz"
   sha256 "8a880a5f2fbff63bbeb17be84d534b2cef02ff68cc7b835adec2cd408972d5b4"
   license ""
 

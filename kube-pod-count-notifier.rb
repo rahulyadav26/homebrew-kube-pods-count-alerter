d45b1504c9733acd29e3e2214e201c9cfbf52f0e
Susheem Koul <susheemkoul97@gmail.com>
2021-05-15 13:36:13 +0530

    Update kube-pod-count-notifier.rb

--- kube-pod-count-notifier.rb
+++ kube-pod-count-notifier.rb
@@ -1,15 +1,15 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class KubePodCountNotifier < Formula
+class Pcc < Formula
   desc ""
   homepage ""
-  url "https://github.com/susheem-k/kube-pod-count-notifier/archive/refs/tags/v0.2.tar.gz"
-  sha256 "e64cbcec1d8ed919bf616b1958fdafc599a3d90793712efeb1e64acf7da83b0e"
+  url "https://github.com/rahulyadav26/pcc/archive/refs/tags/v0.1.tar.gz"
+  sha256 "8a880a5f2fbff63bbeb17be84d534b2cef02ff68cc7b835adec2cd408972d5b4"
   license ""
 
   def install
-    bin.install "kube-pod-count-notifier"
+    bin.install "pcc/podcount"
   end
 
 end
